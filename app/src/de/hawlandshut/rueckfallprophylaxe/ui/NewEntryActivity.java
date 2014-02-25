333257a25fa978387c6d53cb319550f31edc9d22
yelawolf <marius.falke@gmail.com>
2014-02-25 18:45:12 +0100

    Modified NewEntryActivity: - Date field with current date - Modified text field length to fill_parent - Save button

--- app/src/de/hawlandshut/rueckfallprophylaxe/ui/NewEntryActivity.java
+++ app/src/de/hawlandshut/rueckfallprophylaxe/ui/NewEntryActivity.java
@@ -1,15 +1,20 @@
 package de.hawlandshut.rueckfallprophylaxe.ui;
 
+import java.util.Calendar;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.DatePickerDialog;
 import android.support.v4.app.NavUtils;
+import android.text.format.Time;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 
 public class NewEntryActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -21,6 +26,21 @@ public class NewEntryActivity extends Activity {
 		        R.array.mood_array, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		spinner.setAdapter(adapter);
+		
+		
+		// Set date into date picker text field
+		// TODO: Get date of existing entry
+		// TODO: Date picker modal
+		
+		// New entry -> Current date
+		// Create current date string
+		Time currentTime = new Time();
+		currentTime.setToNow();
+		String currentDate = currentTime.format("%d.%m.%Y - %H:%M");
+		
+		// Set datePicker text field to current date string
+		EditText datePickerText = (EditText) findViewById(R.id.entryDateText);
+		datePickerText.setText(currentDate);
 	}
 
 	@Override

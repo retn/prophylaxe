567c5acf71f57ad42a9555e064432bea9cd60497
Anton Zeitzler <azetsubou@googlemail.com>
2014-03-01 15:35:16 +0100

    datenbank und tables erstellt und einfache funktionen zu query, insert, delete, update

--- app/src/de/hawlandshut/rueckfallprophylaxe/db/Database.java
+++ app/src/de/hawlandshut/rueckfallprophylaxe/db/Database.java
@@ -1,4 +1,11 @@
 package de.hawlandshut.rueckfallprophylaxe.db;
+import java.io.File;
+
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+import net.sqlcipher.Cursor;
+import net.sqlcipher.database.SQLiteDatabase;
 
 /**
  * Datenbank-Verbindungs Klasse
@@ -7,6 +14,69 @@ package de.hawlandshut.rueckfallprophylaxe.db;
  * * implementieren
  */
 public class Database {
-	public Database() {
+	private Context context;
+	private SQLiteDatabase sqldatabase;
+	public Database(Context context) {
+		this.context=context;
+		InitializeSQLCipher();
         }
+	
+    private void InitializeSQLCipher() {
+        SQLiteDatabase.loadLibs(context);
+        File databaseFile = context.getDatabasePath("daten.db");
+        if(!databaseFile.exists()){
+        	databaseFile.mkdirs();
+        	
+        }
+        //databaseFile.delete();
+        
+        SQLiteDatabase database = SQLiteDatabase.openOrCreateDatabase(databaseFile, "user", null);
+        this.sqldatabase=database;
+        MyTables tables=new MyTables(sqldatabase);
+    }
+
+	public void insertExample() {
+		
+        sqldatabase.execSQL("insert into t1(a, b) values(?, ?)", new Object[]{"one for the money",
+                                                                        "two for the show"});
+		
+	}
+
+	public void makeExampleQuery() {
+		String query="select * from t1";
+        Cursor c=sqldatabase.rawQuery(query, null);
+        if(c!=null){
+        	int i=0;
+        	if(c.moveToFirst()){
+        		
+        		do{
+        			String text=c.getString(c.getColumnIndex("a"));
+        			i++;
+        			
+        		}
+        		while(c.moveToNext());
+        	}
+        	Log.d("query", ""+i);
+        }
+		
+	}
+
 }
+
+/*
+ * example table+query
+        database.execSQL("create table t1(a , b)");
+        database.execSQL("insert into t1(a, b) values(?, ?)", new Object[]{"one for the money",
+                                                                        "two for the show"});
+        String query="select * from t1";
+        Cursor c=database.rawQuery(query, null);
+        if(c!=null){
+        	if(c.moveToFirst()){
+        		do{
+        			String text=c.getString(c.getColumnIndex("a"));
+        			Toast.makeText(context, text, Toast.LENGTH_LONG).show();;
+        		}
+        		while(c.moveToNext());
+        	}
+        }
+*/

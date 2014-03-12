ce947b92f2ad5a82e9860c1f1159bb42762a05e5
patsimm <patrick.simmelbauer@gmail.com>
2014-03-12 15:12:35 +0100

    added Google Maps api

--- app/src/de/hawlandshut/rueckfallprophylaxe/ui/ContactpointMapActivity.java
+++ app/src/de/hawlandshut/rueckfallprophylaxe/ui/ContactpointMapActivity.java
@@ -1,14 +1,52 @@
 package de.hawlandshut.rueckfallprophylaxe.ui;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class ContactpointMapActivity extends Activity {
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_contactpoint_map);
-	}
-
+	// Google Map
+    private GoogleMap googleMap;
+ 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contactpoint_map);
+ 
+        try {
+            // Loading map
+            initilizeMap();
+ 
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+ 
+    }
+ 
+    /**
+     * function to load map. If map is not created it will create it for you
+     * */
+    private void initilizeMap() {
+        if (googleMap == null) {
+            googleMap = ((MapFragment) getFragmentManager().findFragmentById(
+                    R.id.map)).getMap();
+ 
+            // check if map is created successfully or not
+            if (googleMap == null) {
+                Toast.makeText(getApplicationContext(),
+                        "Sorry! unable to create maps", Toast.LENGTH_SHORT)
+                        .show();
+            }
+        }
+    }
+ 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        initilizeMap();
+    }
 }
